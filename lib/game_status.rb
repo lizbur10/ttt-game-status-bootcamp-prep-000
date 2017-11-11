05c826e3c0632839d0d44a8633d27de480a5a749
Liz Burton <liz.burton147@gmail.com>
2017-11-11 18:54:40 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,32 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+]
+
+def won? (board)
+  WIN_COMBINATIONS.each do |index_array|
+    combo = []
+    index_array.each do |index|
+      combo << board[index]
+    end
+    winning_combo_x = combo.all? do |move|
+      move == "X"
+    end
+    winning_combo_o = combo.all? do |move|
+      move == "O"
+    end
+    if winning_combo_x || winning_combo_o
+      return index_array
+    end
+  end
+  return false
+end
